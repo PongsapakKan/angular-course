d8a7d0cb3d1df4b84ef2e1c5544ffc93ce5eba15
Pongsapak <Pak.Vertice@hotmail.com>
2017-06-26 10:00:06 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyABxeHDDMECfIJLl-aquKX4E1PVlUhtIik",
+      authDomain: "ngshopping-c9ecd.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import * as firebase from "firebase";
 
+@Injectable()
 export class AuthService {
     token: string;
+
+    constructor(private router: Router) { }
 
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -14,9 +19,13 @@ export class AuthService {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
                 response => {
+                    this.router.navigate(['/']);
                     firebase.auth().currentUser.getToken()
                         .then(
-                            (token: string) => this.token = token
+                            (token: string) => {
+                                this.token = token; 
+                                localStorage.setItem('token', token);
+                            }
                         );
                 }
             )
@@ -26,10 +35,11 @@ export class AuthService {
     }
 
     logout() {
+        this.router.navigate(['/']);
         firebase.auth().signOut();
         this.token = null;
     }
-
+    
     getToken() {
         firebase.auth().currentUser.getToken()
             .then(
